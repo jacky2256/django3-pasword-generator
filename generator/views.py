f631ba05e0e2b1377a13e75567b7b624e6d91368
Artur <a.bojic22@gmail.com>
2022-11-03 17:17:44 +0200

    commit one

---
+++ generator/views.py
@@ -0,0 +1,35 @@
+from curses.ascii import HT
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+# Create your views here.
+
+def home(request):
+    return render(request, 'generator/home.html')
+
+def password(request):
+
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+
+    length = int(request.GET.get('length'), 12)
+
+    thepassword = ''
+   
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    
+    if request.GET.get('uppercase'):
+        characters.extend(list('!@#$%^&*()'))
+
+    if request.GET.get('numbers'):
+        characters.extend(list('123456789'))
+
+    for x in range(length):
+        thepassword += random.choice(characters)
+    
+    return render(request, 'generator/password.html', {'password':thepassword})
+
+def about(request):
+    return render(request, 'generator/about.html')
+    
+  
